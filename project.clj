91fb003db110eb24264bf5b4d717ab7936b4da08
Circle CI <circleci@infrablocks.io>
2021-08-15 11:29:21 +0000

    Pre-release version 0.1.2-RC2 [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/component.flyway-migrator "0.1.2-RC1"
+(defproject io.logicblocks/component.flyway-migrator "0.1.2-RC2"
   :description "A component to run flyway migrations on system start."
   :url "https://github.com/logicblocks/component.flyway-migrator"
 
