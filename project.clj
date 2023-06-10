569a909eaadda19c1a920606938305b309fdf090
Toby Clemson <tobyclemson@gmail.com>
2023-06-10 22:16:57 +0100

    Try bumping version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/component.flyway-migrator "0.1.2-RC2"
+(defproject io.logicblocks/component.flyway-migrator "0.1.2-RC3"
   :description "A component to run flyway migrations on system start."
   :url "https://github.com/logicblocks/component.flyway-migrator"
 
